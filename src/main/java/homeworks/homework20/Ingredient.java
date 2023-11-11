8e8c6420f6afb7a98c5819c6e0cf2c61ee97feb2
Artem <artemavanyan7@gmail.com>
2023-11-11 17:55:55 +0200

    master2

---
+++ src/main/java/homeworks/homework20/Ingredient.java
@@ -0,0 +1,14 @@
+package homeworks.homework20;
+
+enum Ingredient {
+    CHEESE(1),
+    TOMATO(3),
+    LETTUCE(5);
+    private double price;
+    Ingredient(double price) {
+        this.price = price;
+    }
+    public double getPrice() {
+        return price;
+    }
+}
