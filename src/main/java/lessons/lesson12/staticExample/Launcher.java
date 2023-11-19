eb1300111d88d977b0fdd0f670ae310d02ac2df9
Artem <artemavanyan7@gmail.com>
2023-11-19 18:38:06 +0200

    homework24

--- src/main/java/lessons/lesson12/staticExample/Launcher.java
+++
@@ -1,9 +0,0 @@
-package lessons.lesson12.staticExample;
-
-public class Launcher {
-
-    public static void main(String[] args) {
-        System.out.println(StaticForFields.name);
-        System.out.println(new StaticForFields().age);
-    }
-}
--- src/main/java/lessons/lesson14/innerclasses/LauncherLocalMethod.java
+++
@@ -1,7 +0,0 @@
-package lessons.lesson14.innerclasses;
-
-public class LauncherLocalMethod {
-    public static void main(String[] args) {
-        new LocalInnerClassMethod().someMethodOfOuterClass();
-    }
-}
