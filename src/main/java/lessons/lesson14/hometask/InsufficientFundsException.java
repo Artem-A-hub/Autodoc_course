910a44daa67dd214bb5be058480ae8627555adb1
Artem <artemavanyan7@gmail.com>
2023-12-09 22:55:45 +0200

    homework29

--- src/main/java/lessons/lesson14/hometask/InsufficientFundsException.java
+++
@@ -1,7 +0,0 @@
-package lessons.lesson14.hometask;
-
-public class InsufficientFundsException extends Exception {
-
-    public InsufficientFundsException() {
-    }
-}
--- src/main/java/lessons/lesson8/oop/common/About.java
+++
@@ -1,9 +0,0 @@
-package lessons.lesson8.oop.common;
-
-public class About {
-
-    public static void main(String[] args) {
-        Person person = new Person();
-        person.aboutPerson();
-    }
-}
