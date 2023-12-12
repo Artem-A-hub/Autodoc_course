a6658333872c8f93294f74bc547b19e79e561c23
Artem <artemavanyan7@gmail.com>
2023-12-12 18:04:46 +0200

    master

---
+++ src/test/java/lesson30/streamApi/workinhWithObject/Car.java
@@ -0,0 +1,20 @@
+package lesson30.streamApi.workinhWithObject;
+
+public class Car {
+
+    private String model;
+    private int year;
+
+    public Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
